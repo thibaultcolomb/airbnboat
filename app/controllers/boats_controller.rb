8f8b4d136b73f1dee69358711820062d2e48e874
Thibault Colomb <thibault.colomb934@gmail.com>
2024-01-29 17:43:04 +0100

    added Boat model, still need to add validation etc.

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,25 @@
+class BoatsController < ApplicationController
+  def index
+    @boats = Boat.all
+  end
+
+  def show
+    @boat = Boat.find(params[:id])
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = current_user.boats.new(boat_params)
+    @boat.save
+    redirect_to boats_path
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:name, :port, :capacity, :category, :price_per_day, :description, :image)
+  end
+end
