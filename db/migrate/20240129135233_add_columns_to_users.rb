bfe69e5047662cf065918db5d79dd2e093011f9f
Thibault Colomb <thibault.colomb934@gmail.com>
2024-01-29 15:00:43 +0100

    created user model, added devise gem

---
+++ db/migrate/20240129135233_add_columns_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :bio, :text
+    add_column :users, :age, :integer
+    add_column :users, :license, :boolean
+  end
+end
