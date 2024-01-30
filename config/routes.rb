1da745eb5720e9c859f2e467dcad9e50f1b876f4
Thibault Colomb <thibault.colomb934@gmail.com>
2024-01-30 15:05:24 +0100

    added dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/my-bookings", to: "bookings#mybookings"
+  get "/dashboard", to: "bookings#index"
 
   resources :boats do
     resources :bookings, only: [:index, :show, :new, :create] do
