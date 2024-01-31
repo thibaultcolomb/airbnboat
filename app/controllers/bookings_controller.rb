6054ef77a5fe9439519e767444fb8265ec8c414b
Harri Besceli <hbesceli@gmail.com>
2024-01-31 11:06:56 +0000

    added approve functionality, added more seeds

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -39,6 +39,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :boat_id, :current_user_id)
+    params.require(:booking).permit(:start_date, :end_date, :boat_id, :current_user_id, :confirmed)
   end
 end
